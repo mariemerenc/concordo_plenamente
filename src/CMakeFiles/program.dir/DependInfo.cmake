0be880987ad7dda137d1025c0c93b088187e6b10
mariemerenc <103092034+mariemerenc@users.noreply.github.com>
2023-07-20 19:35:40 -0300

    Add files via upload

---
+++ src/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mariemerenc/concordo_plenamente-main/src/channel.cpp" "CMakeFiles/program.dir/channel.cpp.o" "gcc" "CMakeFiles/program.dir/channel.cpp.o.d"
+  "/home/mariemerenc/concordo_plenamente-main/src/chtext.cpp" "CMakeFiles/program.dir/chtext.cpp.o" "gcc" "CMakeFiles/program.dir/chtext.cpp.o.d"
+  "/home/mariemerenc/concordo_plenamente-main/src/chvoice.cpp" "CMakeFiles/program.dir/chvoice.cpp.o" "gcc" "CMakeFiles/program.dir/chvoice.cpp.o.d"
+  "/home/mariemerenc/concordo_plenamente-main/src/concordo.cpp" "CMakeFiles/program.dir/concordo.cpp.o" "gcc" "CMakeFiles/program.dir/concordo.cpp.o.d"
+  "/home/mariemerenc/concordo_plenamente-main/src/exec.cpp" "CMakeFiles/program.dir/exec.cpp.o" "gcc" "CMakeFiles/program.dir/exec.cpp.o.d"
+  "/home/mariemerenc/concordo_plenamente-main/src/message.cpp" "CMakeFiles/program.dir/message.cpp.o" "gcc" "CMakeFiles/program.dir/message.cpp.o.d"
+  "/home/mariemerenc/concordo_plenamente-main/src/server.cpp" "CMakeFiles/program.dir/server.cpp.o" "gcc" "CMakeFiles/program.dir/server.cpp.o.d"
+  "/home/mariemerenc/concordo_plenamente-main/src/system.cpp" "CMakeFiles/program.dir/system.cpp.o" "gcc" "CMakeFiles/program.dir/system.cpp.o.d"
+  "/home/mariemerenc/concordo_plenamente-main/src/user.cpp" "CMakeFiles/program.dir/user.cpp.o" "gcc" "CMakeFiles/program.dir/user.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/channel.cpp.o"
+  "CMakeFiles/program.dir/channel.cpp.o.d"
+  "CMakeFiles/program.dir/chtext.cpp.o"
+  "CMakeFiles/program.dir/chtext.cpp.o.d"
+  "CMakeFiles/program.dir/chvoice.cpp.o"
+  "CMakeFiles/program.dir/chvoice.cpp.o.d"
+  "CMakeFiles/program.dir/concordo.cpp.o"
+  "CMakeFiles/program.dir/concordo.cpp.o.d"
+  "CMakeFiles/program.dir/exec.cpp.o"
+  "CMakeFiles/program.dir/exec.cpp.o.d"
+  "CMakeFiles/program.dir/message.cpp.o"
+  "CMakeFiles/program.dir/message.cpp.o.d"
+  "CMakeFiles/program.dir/server.cpp.o"
+  "CMakeFiles/program.dir/server.cpp.o.d"
+  "CMakeFiles/program.dir/system.cpp.o"
+  "CMakeFiles/program.dir/system.cpp.o.d"
+  "CMakeFiles/program.dir/user.cpp.o"
+  "CMakeFiles/program.dir/user.cpp.o.d"
+  "program"
+  "program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
